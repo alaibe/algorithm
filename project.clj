06bbf5765f3d9ded06969cc7266811271fb6cb84
Anthony Laibe <anthony@laibe.cc>
2017-02-05 18:05:08 +0000

    Update project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject algorithm "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject algorithm "1.0.0"
+  :description "A toy project to learn clojure \nand at the same time revised \nsome algorithms"
+  :url "https://github.com/alaibe/algorithm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
