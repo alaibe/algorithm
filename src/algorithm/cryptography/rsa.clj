3c18f39603194398077a18f5e3676124740c1c77
Anthony Laibe <anthony@laibe.cc>
2017-02-05 17:56:31 +0000

    Adding rsa

--- src/algorithm/cryptography/rsa.clj
+++ src/algorithm/cryptography/rsa.clj
@@ -1,6 +1,54 @@
-(ns algorithm.cryptography.rsa)
+(ns algorithm.cryptography.rsa
+  (:require [integer.prime :refer [miller-rabin-prime? power-mod]]))
+
+;TODO: Add bits size
+
+(defn big-int
+  []
+  (+ 1000 (rand-int 1000)))
+
+(defn generate-prime
+  []
+  (loop [x (big-int)]
+    (if (miller-rabin-prime? x)
+      x
+      (recur (big-int)))))
+
+(defn gcd
+  [a b]
+  (if (zero? b)
+    a
+    (recur b (mod a b))))
+
+(defn encode
+  [blocks public-key modulus]
+  (map #(power-mod % public-key modulus) blocks))
+
+(defn decode
+  [encoded private-key modulus]
+  (encode encoded, private-key, modulus))
 
 (defn generate-key-pair
-  [])
+  []
+  (let [p1 (generate-prime)
+        p2 (generate-prime)
+        n (* p1 p2)
+        t (* (dec p1) (dec p2))
+        e (loop [x 2]
+            (if (= 1 (gcd x t))
+              x
+              (recur (inc x))))
+        d (loop [x 1]
+            (if (= 1 (mod (* x e) t))
+              x
+              (recur (inc x))))]
+    [e, d n]))
 
-(println (generate-key-pair))
+(let [[public-key private-key modulus] (generate-key-pair)
+      value [255]
+      encoded (encode value public-key modulus)
+      decoded (decode encoded private-key modulus)]
+  (println [public-key private-key modulus])
+  (println value)
+  (println encoded)
+  (println decoded))
